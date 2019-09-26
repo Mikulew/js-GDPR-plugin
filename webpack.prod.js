cdf95c7fdec9658118bcb24c0a80a8c20c324123
Mikulew <mikulew@gmail.com>
2019-09-26 23:36:22 +0200

    Fix http-server and terser-plugin bug

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const TerserPlugin = require("tester-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
